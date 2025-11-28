07cc0a9931dc2be2f2496f65bff1729c4e39a401
suffle-girl <sarka.flidrova@email.cz>
2025-11-28 10:07:35 +0100

    adding multiple routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,12 @@
 import { Routes } from '@angular/router';
 import { TasksComponent } from './tasks/tasks.component';
+import { NoTaskComponent } from './tasks/no-task/no-task.component';
 
 export const routes: Routes = [
+  {
+    path: '', // <yourdomain>
+    component: NoTaskComponent,
+  },
   {
     path: 'tasks', // <yourdomain>/tasks,
     component: TasksComponent,
