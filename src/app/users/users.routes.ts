1df62a48c6527dc3764c0284aac3291a6b6f11ca
suffle-girl <sarka.flidrova@email.cz>
2025-12-01 14:35:49 +0100

    splitting children routes definition across multiple files

---
+++ src/app/users/users.routes.ts
@@ -0,0 +1,19 @@
+import { Routes } from '@angular/router';
+import { NewTaskComponent } from '../tasks/new-task/new-task.component';
+import { TasksComponent } from '../tasks/tasks.component';
+
+export const userRoutes: Routes = [
+  {
+    path: '',
+    redirectTo: 'tasks',
+    pathMatch: 'prefix',
+  },
+  {
+    path: 'tasks', // <yourdomain>/users/<userId>/tasks
+    component: TasksComponent,
+  },
+  {
+    path: 'tasks/new',
+    component: NewTaskComponent,
+  },
+];
