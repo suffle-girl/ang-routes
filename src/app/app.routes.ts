75154145446347d45895f7530d097f693b052d60
suffle-girl <sarka.flidrova@email.cz>
2025-12-03 17:13:33 +0100

    introduction to route guards - canActivate with a dummy function for demo purposes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,4 @@
-import { Routes } from '@angular/router';
+import { CanMatchFn, RedirectCommand, Router, Routes } from '@angular/router';
 import { NoTaskComponent } from './tasks/no-task/no-task.component';
 import {
   resolveTitle,
@@ -7,6 +7,16 @@ import {
 } from './users/user-tasks/user-tasks.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { userRoutes } from './users/users.routes';
+import { inject } from '@angular/core';
+
+const dummyCanMatch: CanMatchFn = (route, segments) => {
+  const router = inject(Router);
+  const shouldGetAccess = Math.random();
+  if (shouldGetAccess < 0.5) {
+    return true;
+  }
+  return new RedirectCommand(router.parseUrl('/unauthorized'));
+};
 
 export const routes: Routes = [
   {
@@ -18,6 +28,7 @@ export const routes: Routes = [
     path: 'users/:userId', // <yourdomain>/users/<userId>
     component: UserTasksComponent,
     children: userRoutes,
+    canActivate: [dummyCanMatch],
     data: {
       message: 'hello',
     },
